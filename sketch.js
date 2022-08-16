458174b5d02f54b83629f8c56f23d2c33b6e0451
Amber <ambertanaka@yahoo.com>
2022-08-16 12:32:19 -0700

    Made fish slider actually work lol

--- sketch.js
+++ sketch.js
@@ -6,13 +6,6 @@ let fishSlider, lilypadSlider;
 let maxFish = 30;
 let maxPads = 14;
 
-// let Controls = function () {
-//   this.numFish = 30;
-//   this.numPads = 20;
-// };
-
-// let controls = new Controls();
-
 function preload() {
   for (let i = 1; i < 8; i++) {
     fishes[i] = loadImage("assets/fish" + i + ".gif");
@@ -30,19 +23,10 @@ function setup() {
   for (let i = 0; i < maxPads; i++) pond.push(new Lilypad());
   fishSlider = createSlider(0, 80, 30, 2);
   lilypadSlider = createSlider(0, 60, 20, 2);
-  // let gui = new dat.GUI({
-  //   width: 295,
-  // });
-  // gui.add(controls, "numFish", 0, 80).name("Number of Fish").step(1);
-  // for (let i = 0; i < controls.numFish; i++) {
-  //   school.push(new Fish()); //flock.push(new Boid());
-  // }
 }
 
 function draw() {
   background(106, 193, 216);
-  // maxFish = fishSlider.value();
-  // maxPads = lilypadSlider.value();
   for (let food of feast) {
     food.show();
   }
@@ -53,24 +37,26 @@ function draw() {
     fish.update();
     fish.show();
   }
-  // Adjust the amount of boids on screen according to the slider value
-  // let maxFish = controls.numFish;
-  // let difference = school.length - maxFish;
-  // if (difference < 0) {
-  //   for (let i = 0; i < -difference; i++) {
-  //     school.push(new Fish()); // Add boids if there are less boids than the slider value
-  //   }
-  // } else if (difference > 0) {
-  //   for (let i = 0; i < difference; i++) {
-  //     flock.pop(); // Remove boids if there are more boids than the slider value
-  //   }
-  // }
+  // Adjust the amount of fish on screen according to the slider value
+  maxFish = fishSlider.value();
+  // maxPads = lilypadSlider.value();
+  let difference = school.length - maxFish;
+  if (difference < 0) {
+    for (let i = 0; i < -difference; i++) {
+      school.push(new Fish());
+    }
+  } else if (difference > 0) {
+    for (let i = 0; i < difference; i++) {
+      school.pop();
+    }
+  }
   for (let lilypad of pond) {
     lilypad.edges();
     lilypad.update();
     lilypad.show();
   }
 }
+
 function mousePressed() {
   feast.push(new Food(mouseX, mouseY));
 }
@@ -92,4 +78,4 @@ function mousePressed() {
 //       }
 //     }
 //   }
-// };
+// }
